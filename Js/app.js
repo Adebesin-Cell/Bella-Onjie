fb0620d708961f214af979f8ed76a8176df86c3b
Adebesin-Cell <adebesintolulope80@gmail.com>
2021-06-29 14:37:04 +0100

    adding files

---
+++ Js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("Running website!!!!!!");
+});
+
+const menuBtn = document.querySelector(".header__icon");
+
+menuBtn.addEventListener("click", () => {
+  console.log("Menu btn was fired!!!!");
+});
